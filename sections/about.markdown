cc96d8c2eb4a28c92d0d0e2b5c3cc1df48bb5d4c
Alexander Vandenbroucke <alexander.vandenbroucke@kuleuven.be>
2015-12-24 14:33:41 +0100

    Add about section.

--- sections/about.markdown
+++ sections/about.markdown
@@ -1,9 +1,14 @@
 ---
 title: About
+priority: 2
 ---
 Hello, I'm Alexander Vandenbroucke. I recently started a Ph.D. in the
 [Programming Languages Group](http://people.cs.kuleuven.be/~tom.schrijvers/research.html)
 of [Tom Schrijvers](http://people.cs.kuleuven.be/~tom.schrijvers/) at the KU
 Leuven.
 
-### Interests
+<img src="/images/me.jpg" style="width:50%" class="img-rounded"></img>
+
+### <i class="fa fa-info-circle"></i> Interests
+My interests are in programming languages, especially functional programming
+languages and Haskell.
--- site.hs
+++ site.hs
@@ -1,12 +1,14 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
-
+import Data.Monoid (mappend)
+import Hakyll
+import Data.List (sortBy)
+import Text.Read (readMaybe)
+import Data.Ord (Down(Down), comparing)
 
 --------------------------------------------------------------------------------
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith conf $ do
   match "images/*" $ do
     route   idRoute
     compile copyFileCompiler
@@ -19,13 +21,21 @@ main = hakyll $ do
     route idRoute
     compile copyFileCompiler
 
+  match "fonts/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "publications/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
   match "sections/*.markdown" $ do
     compile pandocCompiler
 
   match "index.html" $ do
     route idRoute
     compile $ do
-      sections <- loadAll "sections/*"
+      sections <- highestPriorityFirst =<< loadAll "sections/*"
       let ctx = listField "sections" defaultContext (return sections)
                 `mappend` defaultContext
       getResourceBody
@@ -36,3 +46,16 @@ main = hakyll $ do
   match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
+type Priority = Maybe Int
+
+highestPriorityFirst :: MonadMetadata m => [Item a] -> m [Item a]
+highestPriorityFirst is = do
+  priorities <- mapM (getPriority . itemIdentifier) is
+  return (map fst (sortBy (comparing (Down . snd)) (zip is priorities)))
+
+getPriority :: MonadMetadata m => Identifier -> m Priority
+getPriority i = getMetadataField i "priority" >>= return . (>>= readMaybe)
+
+--------------------------------------------------------------------------------
+conf :: Configuration
+conf = defaultConfiguration { deployCommand = "/bin/bash deploy.sh" }
